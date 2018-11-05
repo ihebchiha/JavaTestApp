e845d19c94f221327b10cf553a3249a3dec9d967
Iheb Chiha <iheb.chiha@proxym-it.com>
2018-11-05 10:12:38 +0100

    Editing MainActivity.java

--- app/src/main/java/javatestapp/ihebchiha/com/javatestapp/MainActivity.java
+++ app/src/main/java/javatestapp/ihebchiha/com/javatestapp/MainActivity.java
@@ -2,12 +2,34 @@ package javatestapp.ihebchiha.com.javatestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText username = findViewById(R.id.txt1);
+        EditText password = findViewById(R.id.txt2);
+
+        username.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                Toast.makeText(getApplicationContext(),username.getText(),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
     }
 }
