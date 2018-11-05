c1c7f546c73b196b6742ff579fcb9b74b68c4326
Iheb Chiha <iheb.chiha@proxym-it.com>
2018-11-05 09:29:02 +0100

    ToGitHub

---
+++ app/src/main/java/javatestapp/ihebchiha/com/javatestapp/MainActivity.java
@@ -0,0 +1,13 @@
+package javatestapp.ihebchiha.com.javatestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
